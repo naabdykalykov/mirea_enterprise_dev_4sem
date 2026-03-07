9b76cd47a62766e699647df9fde24337f81f7493
naabdykalykov <znslt24@gmail.com>
2026-03-07 14:59:18 +0300

    add task 1.2

--- kp-1/main.py
+++ kp-1/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from pathlib import Path
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message":
-"Авторелоад действительно работает"}
+    return {"message": "Авторелоад действительно работает"}
+
+
+TEMPLATES_DIR = Path(__file__).parent / "templates"
+INDEX_HTML = TEMPLATES_DIR / "index.html"
+
+
+@app.get("/first-class", response_class=HTMLResponse)
+def read_first_class_root():
+    return INDEX_HTML.read_text(encoding="utf-8")
