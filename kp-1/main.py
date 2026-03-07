372ebf471c0101640157c0fae889ff750188cef5
naabdykalykov <znslt24@gmail.com>
2026-03-07 15:33:03 +0300

    add task 1.3

--- kp-1/main.py
+++ kp-1/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from pathlib import Path
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class CalculateRequest(BaseModel):
+    num1: float
+    num2: float
 
 
 @app.get("/")
@@ -17,3 +22,7 @@ INDEX_HTML = TEMPLATES_DIR / "index.html"
 @app.get("/first-class", response_class=HTMLResponse)
 def read_first_class_root():
     return INDEX_HTML.read_text(encoding="utf-8")
+
+@app.post("/calculate")
+def calculate(data: CalculateRequest):
+    return {"result": data.num1 + data.num2}
