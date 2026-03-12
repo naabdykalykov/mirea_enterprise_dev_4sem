08154964b2fe713ece7e48cdb498df783bb68ece
naabdykalykov <znslt24@gmail.com>
2026-03-13 00:07:09 +0300

    clear kp-2

--- kp-2/models.py
+++ kp-2/models.py
@@ -1,33 +1,3 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel
 
-
-class User(BaseModel):
-    name: str
-    id: int
-
-
-class UserPayload(BaseModel):
-    name: str
-    age: int
-
-
-class CalculateRequest(BaseModel):
-    num1: float
-    num2: float
-
-FORBIDDEN_WORDS = ("кринж", "рофл", "вайб")
-
-class Feedback(BaseModel):
-    name: str = Field(min_length=2, max_length=50)
-    message: str = Field(min_length=10, max_length=500)
-
-    @field_validator("message")
-    @classmethod
-    def message_no_forbidden_words(cls, v: str) -> str:
-        lower = v.lower()
-        for word in FORBIDDEN_WORDS:
-            if word in lower:
-                raise ValueError("Использование недопустимых слов")
-        return v
-    
 
